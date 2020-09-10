23d21046fd46bfe41828e1b6a7d1ebddeb326506
Thanh <tdn888@gmail.com>
2020-09-10 15:29:24 +1000

    breakdown taskform

---
+++ week7-task-form-javascript/taskform.js
@@ -0,0 +1,44 @@
+// Create a JavaScript function called “validateTaskForm” that verifies that the inputs inserted by the user in the task form are correct:
+
+// Task name -> Not Empty and not longer than 8 characters - 
+// Validation status (valid green border, invalid red border & alert "Task name too long")
+// Replace grey text in input box with instructions "Add task name less than 8 characters" 
+// At end - create invalid alert when submit button pressed and no text provided (> 0) 
+
+
+
+// Description -> Not Empty and not longer than 150 characters 
+// Validation status (valid green border, invalid red border & alert "Description is too long" type length max limit 150)
+// Replace grey text with "Description has to be less than 150 characters"
+// Optional - Update bootstrap to wrap text so that all contents are visible, instead of being in one line
+// At end - create invalid alert when submit button pressed and no text provided (> 0)
+
+
+
+// AssignedTo -> Not Empty and longer than 8 characters
+// Default "Select"
+// If Jamie/Thanh/Alex selected it will give valid green border
+// Create invalid alert when submit button pressed when no dropdown option is selected (only triggered when submit button is pressed)
+
+
+
+// DueDate  -> Not Empty and not in the past
+// Default "current day"
+// When select date it will give valid green border
+// Challenge - Date is invalid when due date before current date is selected, alert "Due date is not valid"
+// Create invalid alert when submit button pressed when you have not entered a due date (only triggered when submit button is pressed)
+
+
+
+// Status checkbox
+// Box the checkbox
+// Can only select 1 option at a time 
+
+
+
+
+// Submit button
+// event.preventDefault in event listener
+// Check everything is valid 
+
+// Verify that your code works as expected
