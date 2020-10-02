7d2f13019d4f88db233e99f7b04beba769b90f73
jamie kim <jkim0116@gmail.com>
2020-10-02 16:19:58 +1000

    Pushing to github after Friday arvo session with nick

--- projectMain - ver1.0/js/index.js
+++ projectMain - ver1.0/js/index.js
@@ -90,17 +90,17 @@ function playPause() {
     if (count == 0) {
         count = 1;
         audio.play();
-        playPauseBTN.innerHTML = "Pause &#9208;";
+        playPauseBTN.innerHTML = "Pause";
     } else {
         count = 0;
         audio.pause();
-        playPauseBTN.innerHTML = "Play &#9658;";
+        playPauseBTN.innerHTML = "Play";
     }
 }
 function stop() {
     playPause()
     audio.pause();
     audio.currentTime = 0;
-    playPauseBTN.innerHTML = "Play &#9658;";
+    playPauseBTN.innerHTML = "Play";
 
 }
